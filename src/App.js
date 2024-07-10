9df77bf9b367636e88465f625500463b0c949af4
Kaan Kani İnce <kkince@newmind.ai>
2024-07-10 09:57:54 +0300

    useOnclickOutside custom hook created

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import UseOnclickOutsideTest from "./components/use-outside-click/test";
+
+function App() {
+    return (
+        <div className="App">
+            {/*Use Onclick Outside Hook Test*/}
+            <UseOnclickOutsideTest/>
+        </div>
+)
+    ;
+}
+
+export default App;
